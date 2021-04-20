693539d4176fc67a6787db16f03787c40ab070c2
Shao Ge <geshao3606@gmail.com>
2021-04-19 21:50:26 -0700

    adding and deleting function finished

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -6,22 +6,27 @@ module.exports = (app) => {
   app.get('/api/notes', (req, res) => res.json(dataBase));
 
   app.post('/api/notes', (req, res) => {
-      dataBase.push(req.body);
-      fs.writeFile('./db/db.json', JSON.stringify(dataBase), (error) =>
+        let id = (dataBase.length).toString();
+        dataBase.push({...req.body,id});
+        fs.writeFile('./db/db.json', JSON.stringify(dataBase), (error) =>
         error ? console.error(error) : console.log('Save!')
-      )
-      res.json(true);
-  });
-
-  app.get('/api/notes/:id', (req,res) => {
-    res.json(dataBase[req.params.id]);
+        )
+        res.json(dataBase);
   });
 
   app.delete('/api/notes/:id', (req,res) => {
-    dataBase.splice(req.params.id, 1);
-    fs.writeFile('./db/db.json', JSON.stringify(dataBase), (error) =>
-        error ? console.error(error) : console.log('Delete!')
-    )
-    res.json(true);
+        dataBase.splice(req.params.id, 1);
+        // console.log((dataBase.length).toString())
+        // console.log(req.params.id)
+        for (let i = 0; i < (dataBase.length).toString(); i++) {
+            if (dataBase[i]['id'] > req.params.id) {
+                dataBase[i]['id'] = (dataBase[i]['id']-1).toString();
+            }
+        }
+        // console.log(dataBase)
+        fs.writeFile('./db/db.json', JSON.stringify(dataBase), (error) =>
+            error ? console.error(error) : console.log('Delete!')
+        )
+        res.json(dataBase);
   });
 };  
